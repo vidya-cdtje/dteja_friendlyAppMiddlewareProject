215906d05e60c70c1f36462d69a17f6b36225dc5
vidhya shankar <38492978+vidya-cdtje@users.noreply.github.com>
2018-07-23 16:48:56 +0530

    Chat Rest controller

---
+++ project2Middleware/src/main/java/com/niit/restControllers/ChatRestController.java
@@ -0,0 +1,21 @@
+package com.niit.restControllers;
+
+import java.util.Date;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.niit.model.Message;
+import com.niit.model.OutputMessage;
+
+@RestController
+public class ChatRestController 
+{
+	@MessageMapping("/chat")
+	@SendTo("/topic/message")
+	public OutputMessage sendMessage(Message message)
+	{
+		return new OutputMessage(message,new Date());
+	}
+}
